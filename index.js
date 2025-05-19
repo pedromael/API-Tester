71cb4479eccc05cefc19a93ccdaff7c4e75050ba
pedro manuel <pedmanue14@gmail.com>
2025-05-19 21:54:11 +0100

    feat: add window control functionality and enhance UI layout

--- index.js
+++ index.js
@@ -13,9 +13,24 @@ app.whenReady().then(() => {
         height: 700,
         minWidth: 800,
         minHeight: 600,
+        frame: false,
         webPreferences: {
-        nodeIntegration: true
+        nodeIntegration: true,
+        contextIsolation: false,
+        preload: path.join(__dirname, 'preload.js')
         }
+    });
+
+    const { ipcMain } = require('electron');
+
+    ipcMain.on('window:minimize', () => {
+        win.minimize();
+    });
+    ipcMain.on('window:maximize', () => {
+        win.isMaximized() ? win.unmaximize() : win.maximize();
+    });
+    ipcMain.on('window:close', () => {
+        win.close();
     });
 
     win.loadFile(path.join(__dirname, 'src', 'index.html'));
--- src/scripts/others.js
+++ src/scripts/others.js
@@ -48,3 +48,14 @@ $(document).ready(function () {
     }
   });
 });
+
+const { shell } = require('electron');
+
+// Adiciona um ouvinte para todos os links
+document.addEventListener('click', function (e) {
+  const target = e.target.closest('a');
+  if (target && target.href.startsWith('http')) {
+    e.preventDefault();
+    shell.openExternal(target.href);
+  }
+});
