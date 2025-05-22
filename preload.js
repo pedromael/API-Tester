69d00ecc69b0fc0215b9881dbf2025873c41fdd8
pedro manuel <pedmanue14@gmail.com>
2025-05-22 23:06:29 +0100

    feat: move link click handler to preload.js for better separation of concerns

--- preload.js
+++ preload.js
@@ -5,3 +5,14 @@ contextBridge.exposeInMainWorld('api', {
   maximize: () => ipcRenderer.send('window:maximize'),
   close: () => ipcRenderer.send('window:close')
 });
+
+const { shell } = require('electron');
+
+// Adiciona um ouvinte para todos os links
+document.addEventListener('click', function (e) {
+  const target = e.target.closest('a');
+  if (target && target.href.startsWith('http')) {
+    e.preventDefault();
+    shell.openExternal(target.href);
+  }
+});
